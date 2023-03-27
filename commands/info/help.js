5ec3caecf35e70bf34a88d5c06249fdc0183781c
Westerath  Fynn (ID) <fwesterath@ethz.ch>
2023-03-27 17:04:47 +0200

    update option help for specific command

--- commands/info/help.js
+++ commands/info/help.js
@@ -1,5 +1,6 @@
 const { ChatInputCommandInteraction, SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 const { sendError } = require('../../helper/util/send');
+const { createHelpEmbed } = require('../../helper/util/help');
 
 module.exports = {
     developer: true,
@@ -17,34 +18,29 @@ module.exports = {
      */
     async execute(interaction, client) {
         await interaction.deferReply();
-        const command = interaction.options.getString('command');
-        const embed = new EmbedBuilder()
-            .setColor(client.config.colors.skyblue)
+        try {
+            const command = interaction.options.getString('command');
+            let embed = new EmbedBuilder()
+                .setColor(client.config.colors.skyblue)
 
-        if (command) {
-            const cmd = client.commands.get(command);
-            if (!cmd) return sendError('Error', 'That command doesn\'t exist!', interaction, client);
+            if (command) {
+                const cmd = client.commands.get(command);
+                if (!cmd) return sendError('Error', 'That command doesn\'t exist!', interaction, client);
+                embed = await createHelpEmbed(client, cmd)
+            } else {
+                embed.setTitle('Kahlifar Bot Help');
+                embed.setDescription('Here are all the commands you can use!');
+                for (const command of client.commands) {
+                    embed.addFields(
+                        { name: command[1].data.name, value: command[1].data.description, inline: true }
+                    );
+                }
+            }
+            interaction.editReply({ embeds: [embed] });
 
-            embed.setTitle(`Help for ${cmd.data.name}`);
-            embed.setDescription(cmd.data.description);
-            let embedString = '';
-            for (const option of cmd.data.options) {
-                embedString += `\`${option.name}\` - ${option.description}\n`;
-            }
-            embed.addFields(
-                { name: 'Options', value: embedString, inline: true }
-            );
-        } else {
-            embed.setTitle('Kahlifar Bot Help');
-            embed.setDescription('Here are all the commands you can use!');
-            for (const command of client.commands) {
-                // embed.addField('Commands', client.commands.map(cmd => `\`${cmd.data.name}\` - ${cmd.data.description}`).join('\n'));
-                embed.addFields(
-                    { name: command[1].data.name, value: command[1].data.description, inline: true }
-                );
-            }
         }
-        interaction.editReply({ embeds: [embed] });
-
+        catch (error) {
+            sendError('Error', error, interaction, client);
+        }
     }
 }
