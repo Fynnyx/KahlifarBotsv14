33eefccf1bebadabdfab3536b243c63ae79746f2
Fynn Westerath <westerathf@gmail.com>
2023-06-16 17:56:37 +0200

    add counting system

--- events/admin/ready.js
+++ events/admin/ready.js
@@ -20,7 +20,6 @@ module.exports = {
             console.info(`\x1b[33m${client.user.username}\x1b[34m, logged in\x1b[0m`)
             client.logger.info(`${client.user.username}, logged in`)
             client.user.setActivity('to you and /help', { type: ActivityType.Listening })
-            console.log(client.guilds.cache.size);
         } catch (error) {
             client.logger.error("Error while ready\n" + error)
         }
