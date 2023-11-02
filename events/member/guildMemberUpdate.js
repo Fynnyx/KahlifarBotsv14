64efdaa0870b4e3868923cf4c70ddafe89e36f44
fwesterath <fwesterath@ethz.ch>
2023-11-02 16:17:57 +0100

    add limiter role management

--- events/member/guildMemberUpdate.js
+++ events/member/guildMemberUpdate.js
@@ -9,6 +9,27 @@ module.exports = {
         try {
             if (member.roles != oldmember.roles) {
                 updateChannels(client)
+
+                // Limiter roles
+                const guild = member.guild;
+                const limiterRolePattern = /^◼+ *- *(.+?) *- *◼+$/;
+                const limiterRoles = guild.roles.cache.filter(role => limiterRolePattern.test(role.name));
+
+                console.log(limiterRoles);
+                if (limiterRoles.size > 0) {
+                    const currentRoles = member.roles.cache;
+                    const highestRole = member.roles.highest;
+
+                    const rolesBelowHighest = limiterRoles.filter(role => role.position < highestRole.position && currentRoles.has(role.id));
+
+                    const missingRoles = limiterRoles.filter(role => !rolesBelowHighest.has(role.id));
+                    console.log(missingRoles);
+                    if (missingRoles.size > 0) {
+                        missingRoles.forEach(role => {
+                            member.roles.add(role)
+                        });
+                    }
+                }
             }
         } catch (error) {
             client.logger.error(error)
