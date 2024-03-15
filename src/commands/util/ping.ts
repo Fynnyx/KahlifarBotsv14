bbd55ba4807f4c3f78b9b2b1306b31f5ae1a80fc
Victor <dubscyber@gmail.com>
2024-03-15 19:05:20 +0100

    add ping command

--- src/commands/util/ping.ts
+++ src/commands/util/ping.ts
@@ -3,11 +3,11 @@ import { CustomCommand } from "@/types/CustomCommand";
 import { ChatInputCommandInteraction, SlashCommandBuilder } from "discord.js";
 
 export default new CustomCommand(
-    true,
+    false, 
     new SlashCommandBuilder()
-        .setName("lol")
-        .setDescription("A test command"),
+        .setName('ping')
+        .setDescription('Get the bots\'s ping'),
     (interaction: ChatInputCommandInteraction, client: CustomClient<true>) => {
-        interaction.reply({ content: "Test command!", ephemeral: true });
+        interaction.reply({ content: `**${client.ws.ping}**ms`, ephemeral: true });
     }
-);
+)
